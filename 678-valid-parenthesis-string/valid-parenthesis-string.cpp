d05213ece7c64cad4643d729a6c9b5d13cd6894c
Sourabh <sourabh24563@iiitd.ac.in>
2026-01-23 21:36:38 +0530

    Time: 38 ms (5.02%) | Memory: 41.1 MB (5.08%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -3,18 +3,17 @@ public:
     bool helper(string s,int idx, int c,vector<vector<int>> &dp){
         if(c<0) return false;
         if(idx==s.size()) return c==0;
-        bool check=false;
+        bool c1,c2,c3;
         if(dp[idx][c]!=-1) return dp[idx][c];
-        if(s[idx]=='(') check=helper(s,idx+1,c+1,dp);
-        else if(s[idx]==')') check= helper(s,idx+1,c-1,dp);
+        if(s[idx]=='(') return helper(s,idx+1,c+1,dp);
+        else if(s[idx]==')') return helper(s,idx+1,c-1,dp);
         else{
-            bool c1=helper(s,idx+1,c+1,dp);
-            bool c2=helper(s,idx+1,c,dp);
-            bool c3=helper(s,idx+1,c-1,dp);
-            check=c1||c2||c3;
+            c1=helper(s,idx+1,c+1,dp);
+            c2=helper(s,idx+1,c,dp);
+            c3=helper(s,idx+1,c-1,dp);
 
         }
-        return dp[idx][c]=check;
+        return dp[idx][c]=c1||c2||c3;
 
     }
     bool checkValidString(string s) {
