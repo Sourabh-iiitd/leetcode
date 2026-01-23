3313b4b94131c09e68a2cdc99290fbb6b2590c45
Sourabh <sourabh24563@iiitd.ac.in>
2026-01-23 22:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (73.85%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    bool helper(string s,int idx, int c,vector<vector<int>> &dp){
-        if(c<0) return false;
-        if(idx==s.size()) return c==0;
-        bool c1,c2,c3;
-        if(dp[idx][c]!=-1) return dp[idx][c];
-        if(s[idx]=='(') return helper(s,idx+1,c+1,dp);
-        else if(s[idx]==')') return helper(s,idx+1,c-1,dp);
-        else{
-            c1=helper(s,idx+1,c+1,dp);
-            c2=helper(s,idx+1,c,dp);
-            c3=helper(s,idx+1,c-1,dp);
-
-        }
-        return dp[idx][c]=c1||c2||c3;
-
-    }
     bool checkValidString(string s) {
-        int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return helper(s,0,0,dp);
+        int min=0;
+        int max=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') {
+                min++;
+                max++;
+            }
+            else if(s[i]==')'){
+                if(min-1<0) min=0;
+                else min--;
+                if(max-1<0) return false;
+                else max--;
+            }
+            else{
+                if(min-1<0) min=0;
+                else min--;
+                max++;
+            }
+        }
+        if(max<0) return false;
+        return min==0;
     }
 };
