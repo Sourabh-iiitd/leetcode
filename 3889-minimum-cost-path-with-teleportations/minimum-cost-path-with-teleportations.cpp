d76320e150b06d6a07a138956eee18bda301679c
Sourabh <sourabh24563@iiitd.ac.in>
2026-01-30 10:47:12 +0530

    Time: 239 ms (74.04%) | Memory: 105.4 MB (77.66%) - LeetSync

--- 3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.cpp
+++ 3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
+//nice vdo hehehehhee
     int minCost(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
-        dp[m-1][n-1] = 0;
+        vector<vector<int>> dp(m, vector<int> (n, INT_MAX));
 
-        int maxVal = 0;
-        for(auto &row : grid) {
-            for(int &val : row) {
-                maxVal = max(maxVal, val);
+        dp[m-1][n-1]=0;
+        int maxi=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                maxi=max(maxi, grid[i][j]);
             }
         }
 
-        vector<int> teleportCost(maxVal+1, INT_MAX);
+        vector<int> telecost(maxi+1, INT_MAX);
 
-        for(int t = 0; t <= k; t++) {
-
-            for(int i = m-1; i >= 0; i--) {
-                for(int j = n-1; j >= 0; j--) {
-
-                    if(i+1 < m)
-                        dp[i][j] = min(dp[i][j], grid[i+1][j] + dp[i+1][j]);
-                    
-                    if(j+1 < n)
-                        dp[i][j] = min(dp[i][j], grid[i][j+1] + dp[i][j+1]);
-
-                    if(t > 0) {
-                        dp[i][j] = min(dp[i][j], teleportCost[grid[i][j]]);
+        for(int t=0 ; t<=k; t++){
+            for(int i=m-1; i>=0 ; i--){
+                for(int j=n-1; j>=0 ;j-- ){
+                    if(i+1<m && dp[i + 1][j] != INT_MAX){
+                        dp[i][j]=min(dp[i][j], dp[i+1][j]+grid[i+1][j]);
+                    }
+                    if(j+1<n && dp[i][j+1] != INT_MAX){
+                        dp[i][j]= min(dp[i][j], grid[i][j+1]+dp[i][j+1]);
+                    }
+                    if(t>0){
+                        dp[i][j]=min(dp[i][j], telecost[grid[i][j]]);
                     }
                 }
             }
-
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n; j++) {
-                    teleportCost[grid[i][j]] = min(teleportCost[grid[i][j]], dp[i][j]);
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    telecost[grid[i][j]]=min(telecost[grid[i][j]], dp[i][j]);
                 }
             }
 
-            for(int i = 1; i < teleportCost.size(); i++) {
-                teleportCost[i] = min(teleportCost[i], teleportCost[i-1]);
+            for(int i=1; i<telecost.size();i++){
+                telecost[i]=min(telecost[i],telecost[i-1]);
             }
         }
-
         return dp[0][0];
     }
 };
