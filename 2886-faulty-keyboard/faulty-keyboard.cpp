3e0f66402baf1d6b8e6ebd1a3d49ee19f5700c67
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-01 00:16:43 +0530

    Time: 4 ms (27.83%) | Memory: 10.2 MB (14.41%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     string finalString(string s) {
         vector<char> arr;
-        for(char x:s){
-            if(x!='i'){
+
+        for (char x : s) {
+            if (x != 'i') {
                 arr.push_back(x);
-            }
-            else{
-                reverse(arr.begin(),arr.end());
+            } else {
+                reverse(arr.begin(), arr.end());
             }
         }
-        int j=0;
-        string ans="";
-        for(int i=0;i<arr.size();i++){
-            ans+=arr[i];
-            // j++;
-        }
-        return ans;
+
+        return string(arr.begin(), arr.end());
     }
 };
