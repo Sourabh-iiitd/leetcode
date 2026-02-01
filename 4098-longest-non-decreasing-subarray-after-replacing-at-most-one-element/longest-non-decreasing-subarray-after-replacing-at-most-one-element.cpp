2c731f35c11fa405db3b5ca47b331963c9e7e7b8
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-01 14:02:46 +0530

    Time: 25 ms (59.63%) | Memory: 190.2 MB (38.98%) - LeetSync

--- 4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element/longest-non-decreasing-subarray-after-replacing-at-most-one-element.cpp
+++ 4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element/longest-non-decreasing-subarray-after-replacing-at-most-one-element.cpp
@@ -2,39 +2,34 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
-        int maxi = 1;
-        vector<int> left(n + 1, 1);
-        vector<int> right(n + 1, 1);
-        
+        if (n <= 1) return n;
+
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
+
         for (int i = 1; i < n; i++) {
             if (nums[i] >= nums[i - 1]) {
-                left[i + 1] = left[i] + 1;
+                left[i] = left[i - 1] + 1;
             }
         }
 
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] <= nums[i + 1]) {
-                right[i + 1] = right[i + 2] + 1;
+                right[i] = right[i + 1] + 1;
             }
         }
 
-        for (int i = 1; i <= n - 1; i++) {
-
-            int val = nums[i - 1];
-
-            int prev = 1e9;
-
-            if (i >= 2) {
-                prev = nums[i - 2];
+        int maxi = 1;
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, left[i]);
+            
+            if (i > 0) {
+                maxi = max(maxi, left[i - 1] + 1);
             }
-
-            maxi = max(maxi, right[i] + 1);
-            maxi = max(maxi, left[i] + 1);
-
-            int next = nums[i];
-
-            if (next >= prev) {
-
+            if (i < n - 1) {
+                maxi = max(maxi, right[i + 1] + 1);
+            }
+            if (i > 0 && i < n - 1 && nums[i - 1] <= nums[i + 1]) {
                 maxi = max(maxi, left[i - 1] + 1 + right[i + 1]);
             }
         }
