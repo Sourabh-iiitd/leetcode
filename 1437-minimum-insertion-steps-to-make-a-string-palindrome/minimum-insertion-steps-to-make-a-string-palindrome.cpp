79b58f7863347183d1f639c7294392d97c30014d
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-04 13:38:52 +0530

    Time: 19 ms (89.36%) | Memory: 9.7 MB (90.12%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int move(string& s, vector<vector<int>>& dp, int idx1, int idx2) {
-        if (idx1 >= idx2) {
-            return 0;
+    int longestCommonSubsequence(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1])
+                    cur[j] = 1 + prev[j - 1];
+                else
+                    cur[j] = max(prev[j], cur[j - 1]);
+            }
+            prev = cur;
         }
-        if (dp[idx1][idx2] != -1) {
-            return dp[idx1][idx2];
-        }
-        if (s[idx1] == s[idx2]) {
-            return dp[idx1][idx2] = 0 + move(s, dp, idx1 + 1, idx2 - 1);
-        }
+        return prev[m];
+    }
 
-        return  dp[idx1][idx2] =1+ min(move(s, dp, idx1 + 1, idx2),
-                                        move(s, dp, idx1, idx2 - 1));
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+
+        int ans = longestCommonSubsequence(s, s2);
+        return ans;
     }
     int minInsertions(string s) {
 
         int n=s.size();
 
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return move(s,dp,0,n-1);
+        int lcp=longestPalindromeSubseq(s);
+        return n-lcp;
     }
 };
