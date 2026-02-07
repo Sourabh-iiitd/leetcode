3b2ae840a46a0ac1b0ccb49440e9ccde80babe38
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-08 00:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (90.57%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        unordered_set<char>st;
-        for(char c:s){
-            st.insert(c);
+        vector<bool> seen(26, false);
+        int cnt = 0;
+
+        for(char c : s){
+            if(!seen[c - 'a']){
+                seen[c - 'a'] = true;
+                cnt++;
+            }
         }
-        return st.size();
+
+        return cnt;
     }
 };
