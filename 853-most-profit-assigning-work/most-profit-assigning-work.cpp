2a16afbe66e4f020690378e0be02de6d51d592eb
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-09 00:17:55 +0530

    Time: 10 ms (72.26%) | Memory: 41.6 MB (63.19%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int ans=0;
-        for(int i=0;i<worker.size();i++){
-            int maxi=INT_MIN;
-            int idx=-1;
-            for(int j=0;j<difficulty.size();j++){
-                if(difficulty[j]<=worker[i]){
-                    if(profit[j]>maxi){
-                        maxi=profit[j];
-                        idx=j;
-                    }
-                }
-            }
-            if(maxi!=INT_MIN){
-                ans+=maxi;
-            }
-            
+
+        int n=difficulty.size();
+        vector<pair<int,int>> jobs;
+        for(int i=0; i < n; i++){
+            jobs.push_back({difficulty[i], profit[i]});
         }
+
+        sort(jobs.begin(), jobs.end());
+
+        for(int i=1; i < n; i++){
+            jobs[i].second=max(jobs[i].second, jobs[i-1].second);
+        }
+
+        int ans=0;
+
+        for(int i=0; i < worker.size(); i++){
+
+            int l=0, r=n-1, idx=-1;
+
+           
+            while(l <= r){
+                int mid=(l+r)/2;
+
+                if(jobs[mid].first <= worker[i]){
+                    idx=mid;
+                    l=mid + 1;
+                }
+                else r=mid - 1;
+            }
+
+            if(idx != -1){
+                ans+=jobs[idx].second;
+            }
+        }
+
         return ans;
-        
     }
 };
