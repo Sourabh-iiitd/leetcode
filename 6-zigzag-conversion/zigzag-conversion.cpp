5bfba689a3aa2992350b560733f44c931f446344
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-10 23:48:52 +0530

    Time: 6 ms (49.87%) | Memory: 14.2 MB (31.73%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-
-        if(numRows == 1 || numRows >= s.size()) return s;
-
+        if(numRows==1 || numRows>=s.size()) return s;
         string ans="";
-        int n = s.size();
-
-        vector<string> mk(numRows, "");
-
-        int c = 0;          
-        int row = 0;
-        bool dir = true;    
-
-        while(c < n){
-
-            mk[row] += s[c];
+        int n=s.size();
+        vector<string> st(numRows,"");
+        bool dir=true;
+        int i=0;
+        int row=0;
+        int c=0;
+        while(c<n){
+            st[row]+=s[i];
+            i++;
             c++;
-
             if(dir){
-                row++;
-                if(row == numRows - 1) dir = false;
+                row++; 
+                if(row==numRows-1) dir=false;
             }
             else{
                 row--;
-                if(row == 0) dir = true;
+                if(row==0) dir=true;
             }
-        }
 
-        for(auto &str : mk){
-            ans += str;
         }
-
+        for(string s:st){
+            ans+=s;
+        }
         return ans;
     }
 };
