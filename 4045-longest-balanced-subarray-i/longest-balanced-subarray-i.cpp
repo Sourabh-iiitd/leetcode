44c1740a1903a1e65ef0bfb87f09a00d99b9543c
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-10 13:57:28 +0530

    Time: 2065 ms (26.63%) | Memory: 522.1 MB (50.32%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
-        int maxLen = 0;
-
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            unordered_set<int> seen;
-            int distinctEvens = 0;
-            int distinctOdds = 0;
-
+            int odd = 0;
+            int ev = 0;
+            unordered_map<int, int> mpp;
             for (int j = i; j < n; j++) {
-                if (seen.find(nums[j]) == seen.end()) {
+                
+                if (mpp[nums[j]] == 0) {
                     if (nums[j] % 2 == 0)
-                        distinctEvens++;
+                        ev++;
                     else
-                        distinctOdds++;
-                    seen.insert(nums[j]);
+                        odd++;
                 }
+                mpp[nums[j]]++;
 
-                if (distinctEvens == distinctOdds) {
-                    maxLen = max(maxLen, j - i + 1);
+                if (odd == ev) {
+                    ans = max(ans, j - i + 1);
                 }
             }
         }
-        return maxLen;
+        return ans;
     }
 };
