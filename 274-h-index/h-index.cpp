bb4e3a1bc1292a739a5a80f32f499df99b85e8eb
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-11 01:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (29.34%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        reverse(citations.begin(),citations.end());
+        int ans = citations.size(); 
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]<i+1) {
+                ans=i;
+                break;
+
+            }
+        }
+        return ans;
+    }
+};
