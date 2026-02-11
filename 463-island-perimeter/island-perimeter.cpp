70b57a80406cb71d6a8e65b98378f3245b1cd1a0
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-12 01:57:01 +0530

    Time: 7 ms (43.03%) | Memory: 108.8 MB (7.61%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-       int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    if(i+1==n) ans++;
-                    if(j+1==m) ans++;
-                    if(i-1<0) ans++;
-                    if(j-1<0) ans++;
-                    if(i+1<n && grid[i+1][j]==0) ans++;
-                    if(j+1<m && grid[i][j+1]==0) ans++;
-                    if(j-1>=0 && grid[i][j-1]==0) ans++;
-                    if(i-1>=0 && grid[i-1][j]==0) ans++;
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
+        
+      
+        bool found = false;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 1){
+                    q.push({i, j});
+                    visited[i][j] = 1;
+                    found = true;
+                    break; 
+                }
+            }
+            if(found) break;
+        }
+        
+        if(!found) return 0; 
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int k=0; k<4; k++){
+                int nrow = row + drow[k];
+                int ncol = col + dcol[k];
+                
+                
+                if(nrow < 0 || nrow >= n || ncol < 0 || ncol >= m || grid[nrow][ncol] == 0) {
+                    ans++;
+                }
+                
+                else if(visited[nrow][ncol] == 0) {
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
                 }
             }
         }
-        return ans;
         
+        return ans;
     }
 };
