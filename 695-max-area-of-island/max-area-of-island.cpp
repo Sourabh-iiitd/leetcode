1d4e114793fa3dfa3e4a4871302603df679fcbba
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-12 01:28:46 +0530

    Time: 12 ms (11.25%) | Memory: 31.9 MB (13.74%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int maxArea = 0;
+        
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == 1 && visited[i][j] == 0) {
+                    int currArea=0;
+                    queue<pair<int,int>> q;
+                    q.push({i, j});
+                    visited[i][j] = 1;
+                    currArea++;
+                    while(!q.empty()) {
+                        int row = q.front().first;
+                        int col = q.front().second;
+                        q.pop();
+                        
+                        for(int k = 0; k < 4; k++) {
+                            int nrow = row + drow[k];
+                            int ncol = col + dcol[k];
+                            
+                            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+                               visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                                visited[nrow][ncol] = 1;
+                                q.push({nrow, ncol});
+                                currArea++;
+                            }
+                        }
+                    }
+                    maxArea=max(currArea,maxArea);
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+};
