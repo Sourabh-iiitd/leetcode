789ffe1e0188cd6074d5752b5257772880847bc7
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-12 00:37:31 +0530

    Time: 16 ms (66.54%) | Memory: 34.8 MB (64.28%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,18 +4,16 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> ans(n, vector<int>(m, 0)); 
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        queue<pair<pair<int,int>, int>> q;
+        vector<vector<int>> ans(n, vector<int>(m, -1)); 
+
+        queue<pair<int,int>> q;
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(mat[i][j] == 0) {
-                    q.push({{i, j}, 0}); 
-                    visited[i][j] = 1; 
-                } else {
-                    visited[i][j] = 0;
-                }
+                    q.push({i, j}); 
+                    ans[i][j] = 0; 
+                } 
             }
         }
         
@@ -23,21 +21,21 @@ public:
         int dcol[] = {0, 1, 0, -1};
         
         while(!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int steps = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
+           
             q.pop();
             
-            ans[row][col] = steps; 
+           
             
             for(int i = 0; i < 4; i++) {
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
                 
                
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == 0) {
-                    visited[nrow][ncol] = 1;
-                    q.push({{nrow, ncol}, steps + 1}); 
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && ans[nrow][ncol]==-1) {
+                    ans[nrow][ncol] = ans[row][col]+1;
+                    q.push({nrow, ncol}); 
                 }
             }
         }
