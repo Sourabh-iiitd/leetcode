753171d2b27c8eb3eb9ce9d8d730abc49fec99dc
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-15 00:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (17.07%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        vector<int> use;
+        for(int i=0;i<words.size();i++){
+            int ans1=0;
+            for(int j=0;j<words[i].size();j++){
+                int val=words[i][j]-'a';
+                ans1=(ans1+weights[val])%26;
+                
+            }
+            use.push_back(ans1);
+        }
+        string ans="";
+        for(int num : use){                             
+            ans.push_back('z' - num);
+        }
+
+        return ans;
+
+        return ans;
+
+    }
+};
