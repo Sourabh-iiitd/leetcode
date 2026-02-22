79cde2aa61195cfbe12261dde5f2c21ba9b79604
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-23 00:57:11 +0530

    Time: 869 ms (23.83%) | Memory: 174 MB (26.90%) - LeetSync

--- 4065-count-sequences-to-k/count-sequences-to-k.cpp
+++ 4065-count-sequences-to-k/count-sequences-to-k.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     int n;
     unordered_map<string, int> mp;
-    int fxn(vector<int>& a, long long k, int i, long long top,
-            long long bottom) {
-        if (i == n) {
-            if (top / bottom == k && top % bottom == 0)
-                return 1;
+    int helper(vector<int>& a, long long k, int i, long long nm,long long dn) {
+        if(i==n) {
+            if (nm/dn==k && nm % dn==0) return 1;
             return 0;
         }
-        long long gcd = __gcd(top, bottom);
+        long long gcd=__gcd(nm, dn);
 
-        // geneating my unique string so i can use it in the memo.
-        string s = to_string(top / gcd) + "X" + to_string(bottom / gcd) + "X" +
-                   to_string(i * 100 + 7) + "XX";
+        //geneating my unique string
+        string s=to_string(nm/gcd)+"X" + to_string(dn/gcd) + "X" +
+                   to_string(i * 100 + 7)+"XX";
 
-        if (mp.find(s) != mp.end())
+        if (mp.find(s)!=mp.end())
             return mp[s];
 
-        int ans = 0;
-        // case 1
-        ans += fxn(a, k, i + 1, top * a[i], bottom);
-        // case 2
-        ans += fxn(a, k, i + 1, top, bottom * a[i]);
-        // case 3
-        ans += fxn(a, k, i + 1, top, bottom);
-        return mp[s] = ans;
+        int ans=0;
+       
+        ans+=helper(a, k, i + 1, nm * a[i], dn);
+       
+        ans+=helper(a, k, i + 1, nm, dn * a[i]);
+
+        ans+=helper(a, k, i + 1, nm, dn);
+        return mp[s]=ans;
     }
     int countSequences(vector<int>& a, long long k) {
         mp.clear();
 
-        n = a.size();
-        return fxn(a, k, 0, 1, 1);
+        n=a.size();
+        return helper(a, k, 0, 1, 1);
     }
 };
