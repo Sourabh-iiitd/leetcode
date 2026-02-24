8e2cc24b41188f0b19aeed9729487530fb787851
Sourabh <sourabh24563@iiitd.ac.in>
2026-02-24 20:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 63.9 MB (67.02%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     int dp[50001][2];
-    int helper(int idx,int buy,vector<int>& prices, int fee){
-        if(idx==prices.size()){
-            return 0;
-        }
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        int profit=INT_MIN;
-        if(buy){
-            int by=-prices[idx]+helper(idx+1,0,prices,fee);
-            int notby=helper(idx+1,1,prices,fee);
-            profit=max(by,notby);
-        }
-        else{
-            int sell=prices[idx]-fee+helper(idx+1,1,prices,fee);
-            int notsell=helper(idx+1,0,prices,fee);
-            profit=max(sell,notsell);
-        }
-        return dp[idx][buy]=profit;
-    }
+    // int helper(int idx,int buy,vector<int>& prices, int fee){
+    //     if(idx==prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][buy]!=-1) return dp[idx][buy];
+    //     int profit=INT_MIN;
+    //     if(buy){
+    //         int by=-prices[idx]+helper(idx+1,0,prices,fee);
+    //         int notby=helper(idx+1,1,prices,fee);
+    //         profit=max(by,notby);
+    //     }
+    //     else{
+    //         int sell=prices[idx]-fee+helper(idx+1,1,prices,fee);
+    //         int notsell=helper(idx+1,0,prices,fee);
+    //         profit=max(sell,notsell);
+    //     }
+    //     return dp[idx][buy]=profit;
+    // }
     int maxProfit(vector<int>& prices, int fee) {
-        memset(dp,-1,sizeof(dp));
-        return helper(0,1,prices,fee);     
+        memset(dp, -1, sizeof(dp));
+        int n = prices.size();
+        dp[n][1]=dp[n][0]=0;
+        
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int j = 0; j < 2; j++) {
+              
+                // int profit = INT_MIN;
+                if (j) {
+                    int by = -prices[idx] + dp[idx + 1][0];
+                    int notby = dp[idx + 1] [1];
+                    dp[idx][j] = max(by, notby);
+                } else {
+                    int sell =
+                        prices[idx] - fee + dp[idx + 1][1];
+                    int notsell = dp[idx + 1][0];
+                    dp[idx][j] = max(sell, notsell);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
-
