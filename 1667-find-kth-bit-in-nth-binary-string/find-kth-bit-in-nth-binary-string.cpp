0f1b0c2d6184fadf85ef768bed35d001fa62f79a
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-03 16:42:30 +0530

    Time: 115 ms (8.22%) | Memory: 123.6 MB (6.83%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    string dp[21];
     string rev(string s){
         reverse(s.begin(), s.end());
         return s;
@@ -15,13 +16,16 @@ public:
     }
 
     string helper(int i){
+        
         if(i == 1) return "0";
-
+        if(dp[i]!="") return dp[i];
         string prev = helper(i-1);
-        return prev + "1" + rev(invert(prev));
+        return dp[i]=prev + "1" + rev(invert(prev));
     }
 
     char findKthBit(int n, int k) {
+        // memset(dp,"",sizeof(dp)); //string obj kelie kam ni krta ..int char etc krta
+        for(int i = 0; i < 21; i++) dp[i] = "";
         string ans = helper(n);
         return ans[k-1];  
     }
