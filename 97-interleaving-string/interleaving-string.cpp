66cbbdc4efc7045a14fb6f9f2431bea54132d318
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-04 16:33:33 +0530

    Time: 42 ms (5.75%) | Memory: 32.8 MB (11.16%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int dp[101][101][201];
-
-    bool helper(int i, int j, int k, string &s1, string &s2, string &s3){
-        
-        if(i == s1.size() && j == s2.size() && k == s3.size())
-            return true;
-
-        if(k >= s3.size())
-            return false;
-
-        if(dp[i][j][k] != -1)
-            return dp[i][j][k];
-
-        bool res = false;
-
-        if(i < s1.size() && s1[i] == s3[k]) {
-            res |= helper(i+1, j, k+1, s1, s2, s3);
+    bool helper(int i, int j,int k,string s1, string s2, string s3){
+        if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
+        if(k>=s3.size()) return false;
+        if( dp[i][j][k]!=-1) return dp[i][j][k];
+        bool res;
+        if(s1[i]==s3[k]) {
+            res=helper(i+1,j,k+1,s1,s2,s3);
         }
-
-        if(j < s2.size() && s2[j] == s3[k]) {
-            res |= helper(i, j+1, k+1, s1, s2, s3);
+        if(res) return true;
+        if(s2[j]==s3[k]) {
+            res=helper(i,j+1,k+1,s1,s2,s3);
         }
-
-        return dp[i][j][k] = res;
+        return dp[i][j][k]=res;
     }
 
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size();
-        int m = s2.size();
-        int k = s3.size();
-
-        if(n + m != k) return false;
-
-        memset(dp, -1, sizeof(dp));
-
+        memset(dp,-1,sizeof(dp));
+        int n=s1.size();
+        int m=s2.size();
+        int k=s3.size();
+        if(m+n!=k) return false;
+        if(s1+s2==s3) return true;
         return helper(0,0,0,s1,s2,s3);
     }
 };
