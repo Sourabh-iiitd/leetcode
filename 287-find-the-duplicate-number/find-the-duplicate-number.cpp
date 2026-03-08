bf03412fb20b544677f2875a2bc4d447994952c9
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-08 21:23:59 +0530

    Time: 7 ms (50.60%) | Memory: 64.9 MB (89.78%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -8,11 +8,9 @@ public:
                 swap(nums[i], nums[nums[i]-1]);
             }
         }
-        int miss=-1;
         int dup=-1;
         for(int i=0;i<n;i++){
             if(nums[i] != i+1) {
-                miss= i+1;
                 dup=nums[i];
             }
         }
