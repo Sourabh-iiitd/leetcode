cd7b5cf6989ffff26aa8416395dbc06b05ca598d
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-14 20:20:44 +0530

    Time: 31 ms (19.44%) | Memory: 41.4 MB (5.06%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
 
-    void helper(int n,vector<string> &ans,string s){
-        if(s.size()==n){
-            ans.push_back(s);
-            return ;
-        }
-        // char prev;
-        // if(!s.empty())  prev=s.back();
-        if(s.empty() || s.back()!='a') helper(n,ans,s+"a");
-        if(s.empty() || s.back()!='b') helper(n,ans,s+"b");
-        if(s.empty() || s.back()!='c') helper(n,ans,s+"c");
+    // void helper(int n,vector<string> &ans,string s){
+    //     if(s.size()==n){
+    //         ans.push_back(s);
+    //         return ;
+    //     }
+    //     // char prev;
+    //     // if(!s.empty())  prev=s.back();
+    //     if(s.empty() || s.back()!='a') helper(n,ans,s+"a");
+    //     if(s.empty() || s.back()!='b') helper(n,ans,s+"b");
+    //     if(s.empty() || s.back()!='c') helper(n,ans,s+"c");
 
-        // return ans;
-    }
+    //     // return ans;
+    // }
+    // string getHappyString(int n, int k) {
+    //     vector<string> ans;
+    //     helper(n,ans,"");
+    //     int sz=ans.size();
+    //     if(k>sz) return "";
+    //     return ans[k-1]; 
+    // }
+
+    //iterative
     string getHappyString(int n, int k) {
-        vector<string> ans;
-        helper(n,ans,"");
-        int sz=ans.size();
-        if(k>sz) return "";
-        return ans[k-1]; 
+
+        vector<vector<string>> dp(n + 1);
+
+        dp[1] = {"a", "b", "c"};
+
+        for(int len = 2; len <= n; len++){
+            for(string s : dp[len - 1]){
+                char last = s.back();
+
+                if(last != 'a') dp[len].push_back(s + "a");
+                if(last != 'b') dp[len].push_back(s + "b");
+                if(last != 'c') dp[len].push_back(s + "c");
+            }
+        }
+
+        if(k > dp[n].size()) return "";
+        return dp[n][k - 1];
     }
 };
