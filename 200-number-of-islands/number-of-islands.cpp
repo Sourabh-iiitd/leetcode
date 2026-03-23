1767968336d69058ea803a0c1c30cabbaa6d7fd5
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-23 13:54:04 +0530

    Time: 26 ms (62.81%) | Memory: 24.2 MB (19.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    void dfs (int i, int j, vector<vector<char>>& grid,vector<vector<int>> &vis ){
-        vis[i][j]=1;
-        int xdirr[4]={-1,0,1,0};
-        int ydirr[4]={0,1,0,-1};
-
-        for(int d=0;d<4;d++){
-            int nxdir=i+xdirr[d];
-            int nydir=j+ydirr[d];
-            if(nxdir>=0 && nydir>=0 && nxdir<grid.size() && nydir<grid[0].size() && grid[nxdir][nydir]=='1' && vis[nxdir][nydir]==0){
-                dfs(nxdir, nydir, grid, vis);
-            }
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    dfs(i,j,grid,vis);
-                    ans++;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int ans = 0;
+
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == '1' && !vis[i][j]) {
+                    ans++; // new island found
+
+                    queue<pair<int,int>> q;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+
+                    while(!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+
+                        for(int k = 0; k < 4; k++) {
+                            int nx = x + dx[k];
+                            int ny = y + dy[k];
+
+                            if(nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                               grid[nx][ny] == '1' && !vis[nx][ny]) {
+                                
+                                vis[nx][ny] = 1;
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
                 }
             }
         }
         return ans;
-    
-
     }
 };
