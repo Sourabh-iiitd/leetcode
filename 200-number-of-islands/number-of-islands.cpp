70d9a7037c67a60cec3211d165f9da03db75a415
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-23 13:36:28 +0530

    Time: 18 ms (97.92%) | Memory: 17.3 MB (48.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
-    void bfs(int i, int j,vector<vector<char>>& grid, vector<vector<int>> &visited ){
-        
-        visited[i][j]=1;
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        int xdrr[4]={-1,0,1,0};
-        int ydrr[4]={0,1,0,-1};
-        while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int rn=r+xdrr[i];
-                int yn=c+ydrr[i];
-                if(rn >= 0 && yn >= 0 && rn < grid.size() && yn < grid[0].size() 
-   && !visited[rn][yn] && grid[rn][yn] == '1') {
-    
-                    visited[rn][yn] = 1;
-                    q.push({rn, yn});
-                }
+    void dfs (int i, int j, vector<vector<char>>& grid,vector<vector<int>> &vis ){
+        vis[i][j]=1;
+        int xdirr[4]={-1,0,1,0};
+        int ydirr[4]={0,1,0,-1};
+
+        for(int d=0;d<4;d++){
+            int nxdir=i+xdirr[d];
+            int nydir=j+ydirr[d];
+            if(nxdir>=0 && nydir>=0 && nxdir<grid.size() && nydir<grid[0].size() && grid[nxdir][nydir]=='1' && vis[nxdir][nydir]==0){
+                dfs(nxdir, nydir, grid, vis);
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        
         int ans=0;
-        vector<vector<int>> visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                char s=grid[i][j];
-                
-                if(!visited[i][j] && s=='1'){
-                    bfs(i,j,grid,visited);
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i,j,grid,vis);
                     ans++;
                 }
             }
         }
-
         return ans;
+    
 
     }
 };
