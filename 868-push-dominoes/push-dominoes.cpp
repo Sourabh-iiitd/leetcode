be50a55be8ae5e10cc79a54bec0c8722ca2d21e8
Sourabh <sourabh24563@iiitd.ac.in>
2026-03-31 12:51:14 +0530

    Time: 23 ms (40.57%) | Memory: 29.1 MB (9.06%) - LeetSync

--- 868-push-dominoes/push-dominoes.cpp
+++ 868-push-dominoes/push-dominoes.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     string pushDominoes(string dominoes) {
-        int n = dominoes.size();
-        vector<int> left(n, 0), right(n, 0);
+        int n=dominoes.size();
+        vector<int> left(n,0), right(n,0);
 
-    
-        int force = 0;
-        for(int i = 0; i < n; i++){
-            if(dominoes[i] == 'R') force = n;
-            else if(dominoes[i] == 'L') force = 0;
-            else force = max(force - 1, 0);
-            right[i] = force;
+        int f=0;
+        for(int i=0;i<n;i++){
+            if(dominoes[i]=='R') f=n;
+            else if(dominoes[i]=='L') f=0;
+            
+            else f=max(f-1,0);
+            right[i]=f;
+            
         }
-
-        force = 0;
-        for(int i = n - 1; i >= 0; i--){
-            if(dominoes[i] == 'L') force = n;
-            else if(dominoes[i] == 'R') force = 0;
-            else force = max(force - 1, 0);
-            left[i] = force;
+        f=0;
+        for(int i=n-1;i>=0;i--){
+            if(dominoes[i]=='L') f=n;
+            else if(dominoes[i]=='R') f=0;
+            
+            else f=max(f-1,0);
+            left[i]=f;
+            
         }
-
-        
-        string ans = "";
-        for(int i = 0; i < n; i++){
-            if(left[i] == right[i]) ans += '.';
-            else if(right[i] > left[i]) ans += 'R';
-            else ans += 'L';
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(left[i]==right[i]) ans+='.';
+            else if(left[i]>right[i]) ans+='L';
+            else ans+='R';
         }
-
         return ans;
     }
 };
