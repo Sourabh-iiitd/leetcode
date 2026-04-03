8180784d1688b11ca746d3a68300e4a1be5f315b
Sourabh <sourabh24563@iiitd.ac.in>
2026-04-04 01:29:58 +0530

    Time: 39 ms (19.10%) | Memory: 106.7 MB (81.78%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        
+
+        for (auto a : asteroids) {
+            if (mass < a)
+                return false;
+            mass += a;
+        }
+        return true;
+    }
+};
