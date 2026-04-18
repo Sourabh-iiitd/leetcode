376e5e039a927059697458ef8e80f5a06bbbb6e6
Sourabh <sourabh24563@iiitd.ac.in>
2026-04-18 11:02:49 +0530

    Time: 243 ms (41.59%) | Memory: 59.4 MB (92.93%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        if (source == target) {
+        if (source==target) {
             return 0;
         }
 
         unordered_map<int, vector<int>> adj;
-        
-        for (int route = 0; route < routes.size(); route++) {
-            for (auto stop : routes[route]) {
-                adj[stop].push_back(route);
+       
+        for(int i=0;i<routes.size();i++){
+            for(int j=0;j<routes[i].size();j++){
+                int stop=routes[i][j];
+                adj[stop].push_back(i);
             }
         }
 
@@ -18,34 +19,34 @@ public:
         
         for (auto route : adj[source]){
             que.push(route);
-            visited[route] = true;
+            visited[route]=true;
         }
 
-        int busCount = 1;
+        int ans=1;
         while (!que.empty()) {
-            int size = que.size();
+            int size=que.size();
 
             while(size--) {
-                int route = que.front();
+                int route=que.front();
                 que.pop();
 
                 
                 for (auto &stop: routes[route]) {
                     
                     if (stop == target) {
-                        return busCount;
+                        return ans;
                     }
 
                     
                     for (auto nextRoute : adj[stop]) {
                         if (!visited[nextRoute]) {
-                            visited[nextRoute] = true;
+                            visited[nextRoute]=true;
                             que.push(nextRoute);
                         }
                     }
                 }
             }
-            busCount++;
+            ans++;
         }
         return -1;
     }
