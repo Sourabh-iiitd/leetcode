bbeac6227a554f5dd09e42c31cf0f8ae8549f44d
Sourabh <sourabh24563@iiitd.ac.in>
2026-05-04 22:00:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.00%) - LeetSync

---
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool isPrime(int x) {
+        if (x <= 1) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+
+        for (int i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+    int reverseNum(int n) {
+        int r = 0;
+        while (n > 0) {
+            r = r * 10 + (n % 10);
+            n /= 10;
+        }
+        return r;
+    }
+
+    int sumOfPrimesInRange(int n) {
+        int r = reverseNum(n);
+
+        int low = min(n, r);
+        int high = max(n, r);
+
+        int sum = 0;
+
+        for (int i = low; i <= high; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+};
