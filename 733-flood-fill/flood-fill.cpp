647062e778577b4479f8ea934203fa7d204740e6
Sourabh <sourabh24563@iiitd.ac.in>
2026-06-29 21:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (45.61%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    void dfs( int sr, int sc, int color,vector<vector<int>>& image,
-    vector<vector<int>>& vis){
+    int n,m;
+    void dfs(int sr, int sc, int clr,vector<vector<int>>& image, vector<vector<int>> &vis, vector<vector<int>>& ans ){
         vis[sr][sc]=1;
-        int init_color=image[sr][sc];
-        image[sr][sc]=color;
+        int val=image[sr][sc];
+        ans[sr][sc]=clr;
+
+
         int xdir[4]={-1,0,1,0};
         int ydir[4]={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int nx=sr+xdir[i];
-            int ny=sc+ydir[i];
-            if(nx>=0 && ny>=0 && nx<image.size() && ny<image[0].size()&& image[nx][ny]==init_color && !vis[nx][ny]){
 
-                dfs(nx,ny,color,image,vis);
+        for(int k=0;k<4;k++){
+            int nx=sr+xdir[k];
+            int ny=sc+ydir[k];
+            if(nx<n && ny<m && nx>=0 && ny>=0 && vis[nx][ny]!=1 && image[nx][ny]==val){
+                dfs(nx,ny,clr,image,vis,ans);
+                
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        dfs(sr,sc,color,image,vis);
-        return image;
+        n=image.size();
+        m=image[0].size();
+        vector<vector<int>> ans=image;
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        dfs(sr,sc,color,image,vis,ans);
+        return ans;
+        
+
     }
 };
