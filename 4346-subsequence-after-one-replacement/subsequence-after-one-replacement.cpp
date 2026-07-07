1170673f97ec603fcf7d1887fc79bef25b1b4cdc
Sourabh <sourabh24563@iiitd.ac.in>
2026-07-07 20:53:43 +0530

    Time: 23 ms (37.19%) | Memory: 42.3 MB (11.81%) - LeetSync

--- 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
@@ -1,67 +1,69 @@
 class Solution {
 public:
-    bool isSubsequence(string& s, string& t) {
+    bool sub(string s, string t){
         int i=0;
         int j=0;
         while(i<s.size() && j<t.size()){
-            if(s[i]==t[i]){
+            if(s[i]==t[j]){
                 i++;
             }
             j++;
         }
-        return i==s.size(); 
+        return i==s.size();
+
     }
     bool canMakeSubsequence(string s, string t) {
         if(s.size()>t.size()) return false;
-        if(isSubsequence(s,t)) return true;
+        if(sub(s,t)) return true;
 
         int n=s.size();
         int m=t.size();
 
-        vector<int> left(n, -1);
-        vector<int> right(n, m);
+        vector<int> left(n ,-1);
+        vector<int> right(n , m);
 
+        int j=0;
+        for(int i=0;i<n;i++){
+            while(j<m && t[j]!=s[i]){
+                j++;
 
-        int p1 = 0;
-        for (int i = 0; i < n; i++) {
-            while (p1 < m && t[p1] != s[i]) {
-                p1++;
             }
-            if (p1 < m) {
-                left[i] = p1;
-                p1++; 
-            } else {
-                break; 
+            if(j<m){
+                left[i]=j;
+                j++;
+            }
+            else{
+                break;
             }
         }
+        int j2=m-1;
+        for(int i=n-1;i>=0;i--){
+            while(j2>=0 && t[j2]!=s[i]){
+                j2--;
 
-    
-        int p2 = m - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            while (p2 >= 0 && t[p2] != s[i]) {
-                p2--;
             }
-            if (p2 >= 0) {
-                right[i] = p2;
-                p2--; 
-            } else {
+            if(j2>=0){
+                right[i]=j2;
+                j2--;
+            }
+            else{
                 break;
             }
         }
 
+        for(int i=0;i<n;i++){
+            int L=(i == 0) ? -1 : left[i - 1];
+            int R=(i == n - 1) ? m : right[i + 1];
+            
+            if((i>0 && left[i-1]==-1) || (i<n-1 && right[i+1]==m) ) continue;
 
-        for (int i = 0; i < n; i++) {
-            int L = (i == 0) ? -1 : left[i - 1];
-            int R = (i == n - 1) ? m : right[i + 1];
+            if (R - L > 1) return true;
 
-            if ((i == 0 || L != -1) && (i == n - 1 || R != m)) {
-                if (R - L > 1) {
-                    return true;
-                }
-            }
         }
 
         return false;
+
+
 
     }
 };
