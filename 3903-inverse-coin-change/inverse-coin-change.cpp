8541552d40bca5352ece72a031bad1d5cf3d6fc7
Sourabh <sourabh24563@iiitd.ac.in>
2026-07-12 16:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (44.32%) - LeetSync

--- 3903-inverse-coin-change/inverse-coin-change.cpp
+++ 3903-inverse-coin-change/inverse-coin-change.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int ways(vector<int> &ans, int amt, int idx) {
-        if (amt == 0) return 1;
-        if (amt < 0 || idx == ans.size()) return 0;
-
-        if (dp[amt][idx] != -1)
-            return dp[amt][idx];
-
-        int inc = ways(ans, amt - ans[idx], idx);
-        int exc = ways(ans, amt, idx + 1);
-
-        return dp[amt][idx] = inc + exc;
-    }
-
     vector<int> findCoins(vector<int>& numWays) {
-        vector<int> ans;
-
-        for (int i = 0; i < numWays.size(); i++) {
-            int amt = i + 1;
-
-            dp.assign(amt + 1, vector<int>(ans.size() + 1, -1));
-            int wy = ways(ans, amt, 0);
-
-            if (wy < numWays[i]) {
-                ans.push_back(amt);
-
-                dp.assign(amt + 1, vector<int>(ans.size() + 1, -1));
-                wy = ways(ans, amt, 0);
+        int n = numWays.size();
+        vector<int> coins;
+        vector<long long> dp(n+1,0);
+        dp[0]=1;
+        for(int coin=1;coin<=n;++coin)
+        {
+            if(dp[coin]>numWays[coin-1]) return {};
+            if(dp[coin]<numWays[coin-1])
+            {
+                coins.push_back(coin);
+                for(int s=coin;s<=n;++s)
+                {
+                    dp[s] += dp[s-coin];
+                }
             }
-
-            if (wy != numWays[i])
-                return {};
         }
-
-        return ans;
+        for(int i=1;i<=n;++i)
+        {
+            if(dp[i]!=numWays[i-1]) return {};
+        }
+        return coins;
     }
 };
