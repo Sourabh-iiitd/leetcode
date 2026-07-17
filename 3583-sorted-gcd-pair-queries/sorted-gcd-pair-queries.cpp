10a5006ef3cc4782d5f431a2fdedbc70d38a0fc4
Sourabh <sourabh24563@iiitd.ac.in>
2026-07-17 21:28:53 +0530

    Time: 24 ms (100.00%) | Memory: 123.1 MB (82.67%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -1,33 +1,33 @@
 class Solution {
     using ll = long long;
 public:
-    vector<int> gcdValues(vector<int>& A, vector<long long>& queries) {
-        int mx = ranges::max(A);
-        vector<int> freq(mx + 1, 0);
-        vector<ll> GCD(mx + 1, 0);
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int max = ranges::max(nums);
+        vector<int> freq(max + 1, 0);
+        vector<ll> gcdd(max + 1, 0);
         
-        for (const int a : A) {
-            freq[a]++;
+        for (const int num : nums) {
+            freq[num]++;
         }
         
        
-        for (int i = mx; i > 0; i--) {
+        for (int i = max; i > 0; i--) {
             ll sm = 0;
             ll extra = 0;
-            for (int j = i; j <= mx; j += i) {
+            for (int j = i; j <= max; j += i) {
                 sm += freq[j];
-                extra += GCD[j];
+                extra += gcdd[j];
             }
-            GCD[i] = sm * (sm - 1) / 2 - extra;
+            gcdd[i] = sm * (sm - 1) / 2 - extra;
         }
         
        
-        partial_sum(GCD.begin() + 1, GCD.end(), GCD.begin() + 1);
+        partial_sum(gcdd.begin() + 1, gcdd.end(), gcdd.begin() + 1);
         
-       
+    
         vector<int> res(queries.size());
         for (size_t i = 0; i < queries.size(); i++) {
-            res[i] = ranges::upper_bound(GCD, queries[i]) - GCD.begin();
+            res[i] = ranges::upper_bound(gcdd, queries[i]) - gcdd.begin();
         }
             
         return res;
