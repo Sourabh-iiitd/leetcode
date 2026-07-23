b464b50db774850f9748fac2044f8272ed768598
Sourabh <sourabh24563@iiitd.ac.in>
2026-07-24 00:05:36 +0530

    Time: 379 ms (89.47%) | Memory: 48.7 MB (64.33%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int m = 0;
-        for (int v : nums) {
-            m = max(m, v);
-        }
-        int u = 1;
-        while (u <= m) {
-            u <<= 1;
-        }
-        vector<int> s(u);
+
         int n = nums.size();
+        int mx = 0;
+        for (auto x : nums)
+            mx = max(mx, x);
+
+      
+        int u = 1;
+        while (u <= mx)
+            u <<= 1;
+
+        vector<int> s(u);
+
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 s[nums[i] ^ nums[j]] = 1;
             }
         }
+
         vector<int> t(u);
-        for (int x = 0; x < u; x++) {
-            if (!s[x]) {
+
+        for (int i = 0; i < u; i++) {
+            if (s[i] == 0)
                 continue;
-            }
-            for (int v : nums) {
-                t[x ^ v] = 1;
-            }
+
+            for (auto x : nums)
+                t[x ^ i] = 1;
         }
+
         int ans = 0;
-        for (int x = 0; x < u; x++) {
-            if (t[x]) {
+        for (int i = 0; i < u; i++) {
+            if (t[i])
                 ans++;
-            }
         }
+
         return ans;
     }
 };
