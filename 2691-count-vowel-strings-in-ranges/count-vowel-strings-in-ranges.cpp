1c75863ac28ec55a7d4cedda098c16ca61bf0b65
Sourabh <sourabh24563@iiitd.ac.in>
2026-08-03 02:54:03 +0530

    Time: 4 ms (64.39%) | Memory: 70 MB (56.40%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n = words.size();
-        vector<int> Prefix(n + 1, 0);
+        vector<int> pref(n + 1, 0);
         unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'}; 
         for (int i = 0; i < n; i++) {
-            Prefix[i + 1] = Prefix[i];
+            pref[i+1] = pref[i];
             if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
-                Prefix[i + 1]++;  
+                pref[i+1]++;  
             }
         }
-        vector<int> ANS; 
+        vector<int> ans; 
         for (auto& query : queries) {
-            int L = query[0], R = query[1];  
-            ANS.push_back(Prefix[R + 1] - Prefix[L]);  
+            int l = query[0], r= query[1];  
+            ans.push_back(pref[r+1] - pref[l]);  
         }
 
-        return ANS;  
+        return ans;  
     }
 };
