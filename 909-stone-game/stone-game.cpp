ef54f8c4b10ee20cc4260f75149073d3870c719a
Sourabh <sourabh24563@iiitd.ac.in>
2026-08-03 20:53:08 +0530

    Time: 615 ms (5.13%) | Memory: 11.5 MB (38.63%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    int memo[501][501];
+    int helper(int st, int end, vector<int>& piles){
+        if(st==end) return piles[st];
+        int pickst = piles[st] - helper( st + 1, end,piles);
+        
+       if (memo[st][end] != -1) {
+            return memo[st][end];
+        }
+        int pickEnd = piles[end] - helper( st, end - 1,piles);
+
+       
+        return memo[st][end] = max(pickst, pickEnd);
+
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(memo,-1,sizeof(memo));
+        return helper(0,piles.size()-1, piles)>=0;
     }
 };
