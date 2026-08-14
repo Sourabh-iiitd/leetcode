f3a7d8bafc36941e217aaf8f7c2e28232b3b73d6
Sourabh <sourabh24563@iiitd.ac.in>
2026-08-14 09:32:49 +0530

    Time: 1 ms (57.72%) | Memory: 9.7 MB (44.69%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
+        int n=s.size();
         unordered_map<char, int> freq;
-        int left = 0;
-        int maxLen = 0;
-
-        for (int right = 0; right < s.length(); ++right) {
-            // Add current character to map
-            freq[s[right]]++;
-
-            // If frequency exceeds 2, shrink window from left
-            while (freq[s[right]] > 2) {
-                freq[s[left]]--;
-                left++;
+        int ans=0;
+        int l=0;
+        for(int i=0;i<n;i++) {
+            freq[s[i]]++;
+            while(freq[s[i]]>2){
+            
+                freq[s[l]]--;
+                l++;
             }
-
-            // Update maximum valid length
-            maxLen = max(maxLen, right - left + 1);
+            ans=max(ans, i-l+1);
         }
-
-        return maxLen;
+        return ans;
     }
 };
