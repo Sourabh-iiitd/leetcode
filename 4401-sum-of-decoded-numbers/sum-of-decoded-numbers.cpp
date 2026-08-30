f0f33f2d818a736c23fb55cef0764afc17f7e3fe
Sourabh <sourabh24563@iiitd.ac.in>
2026-08-30 22:31:14 +0530

    Time: 161 ms (11.45%) | Memory: 116.5 MB (6.08%) - LeetSync

--- 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.cpp
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    static const long long MOD = 1e9 + 7;
+    static const long long MOD=1e9+7;
 
-    long long power(long long x, long long y) {
-        long long ans = 1;
-        x %= MOD;
+    long long power(long long x,long long y){
+        long long ans=1;
+        x%=MOD;
 
-        while (y > 0) {
-            if (y % 2 == 1) {
-                ans = (ans * x) % MOD;
+        while(y>0){
+            if(y%2==1){
+                ans=(ans*x)%MOD;
             }
 
-            x = (x * x) % MOD;
-            y /= 2;
+            x=(x*x)%MOD;
+            y/=2;
         }
 
         return ans;
     }
 
-    int sumDecoded(vector<long long>& nums) {
-        long long ans = 0;
+    int sumDecoded(vector<long long>& nums){
+        long long ans=0;
 
-        for (long long num : nums) {
-            int w = num % 10;
-            long long d = num / 10;
+        for(long long num:nums){
+            int w=num%10;
+            long long d=num/10;
 
-            string s = to_string(d);
+            string s=to_string(d);
 
-            long long x = stoll(s.substr(0, w));
-            long long y = stoll(s.substr(w));
+            long long x=stoll(s.substr(0,w));
+            long long y=stoll(s.substr(w));
 
-            ans = (ans + power(x, y)) % MOD;
+            ans=(ans+power(x,y))%MOD;
         }
 
         return ans;
