f84fc051f274b9c25b4941b0e4b4c5c6ee469fbc
Sourabh <sourabh24563@iiitd.ac.in>
2026-09-05 13:59:27 +0530

    Time: 76 ms (54.38%) | Memory: 91.2 MB (60.73%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int n;
+    int getNext(vector<vector<int>> &arr, int st, int currEnd){
+        int l=st;
+        int r=n-1;
+        int next=n;
+        while(l<=r){
+            int m= l+(r-l)/2;
+            if(arr[m][0]>= currEnd){
+                next=m;
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return next;
+    }
+
+    int dp[50004];
+    int solve(vector<vector<int>> &arr, int idx){
+        if(idx>=n) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int ntake=solve(arr, idx+1);
+
+        int next=getNext(arr, idx+1, arr[idx][1]);
+        int take= arr[idx][2]+ solve(arr,next);
+
+        return dp[idx]=max(ntake,take);
+    }
+
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        this-> n=profit.size();
+        vector<vector<int>> arr(n,vector<int>(3,0));
+
+        for(int i=0;i<n;i++){
+            arr[i][0]=startTime[i];
+            arr[i][1]=endTime[i];
+            arr[i][2]=profit[i];
+        }
+
+        sort(arr.begin(), arr.end());
+        memset(dp,-1,sizeof(dp));
+        return solve(arr,0);
+    }
+};
