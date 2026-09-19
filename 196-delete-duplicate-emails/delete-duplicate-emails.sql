06c8ca51d0db79557e7024bc3b9dd4927711f693
Sourabh <sourabh24563@iiitd.ac.in>
2026-09-20 00:57:01 +0530

    Time: 994 ms (33.40%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,2 +1,6 @@
-# Write your MySQL query statement below
-delete p1 from Person p1, Person p2 where p1.email=p2.email and p1.id>p2.id;
+DELETE p1 
+FROM Person p1
+JOIN Person p2 
+  ON p1.email = p2.email 
+ WHERE p1.id > p2.id;
+
