b1703f6473cd04db3074e4340e55400be05dcf3e
Sourabh <sourabh24563@iiitd.ac.in>
2026-09-20 09:09:03 +0530

    Time: 514 ms (98.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+SELECT 
+    ROUND(
+        COUNT(a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 
+        2
+    ) AS fraction
+FROM (
+    SELECT 
+        player_id, 
+        MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) AS first_logins
+JOIN Activity a
+  ON first_logins.player_id = a.player_id 
+ AND a.event_date = DATE_ADD(first_logins.first_login, INTERVAL 1 DAY);
