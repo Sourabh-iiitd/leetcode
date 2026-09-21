b122e55d01f58dfa3444651151a8318b6716c4d0
Sourabh <sourabh24563@iiitd.ac.in>
2026-09-21 09:19:00 +0530

    Time: 8 ms (37.50%) | Memory: 36.5 MB (37.50%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
@@ -6,7 +6,7 @@ public:
 
         int ans=0;
         
-        for(int i=0;i<intervals.size();i++){  
+        for(int i=0;i<intervals.size();i++){   //s1 e1  s2 e2  max(s1,s2) <min(e1,e2)
             for(int j=i+1;j<intervals.size();j++){
                 if(max(intervals[i][0], intervals[j][0]) <= min(intervals[i][1], intervals[j][1])) ans++;
             }
